e570b106394ac142472555e5f3a15e7984516a66
SahilSinghal1312 <sahil25csu383@ncuindia.edu>
2026-02-07 15:43:34 +0530

    Add files via upload

---
+++ 19.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n1,n2;
+    cin>>n1>>n2;
+    
+
+    for(int i=n1;i<=n2;i++){
+        int flag=1;
+        for(int j=2;j<i;j++){
+            if(i%j==0){
+                flag=0;
+                break;
+            }
+        }
+         if(flag==1){
+        cout<<i<<endl;
+    }   
+    }
+    return 0;
+}
